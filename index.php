421c360f839c679417907d27df2b454671fd10cb
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-01-03 12:59:47 +0700

    del class

--- index.php
+++ index.php
@@ -44,6 +44,54 @@ if ($text == '/start' || $text == '/start@fadhil_riyanto_bot') {
 	$content = array('chat_id' => $chat_id, 'text' => $reply);
 	$telegram->sendMessage($content);
 	exit;
+} elseif ('/ch_serv' == $adanParse[0] || '/ch_serv@fadhil_riyanto_bot' == $adanParse[0]) {
+	if ($userID == $userid_pemilik) {
+		if ($adanParse[1] == null) {
+			$reply = 'Welcome to server modifier!' . PHP_EOL . PHP_EOL . 'Write --all for see all serv' . PHP_EOL . 'Write /ch_serv {num} For select';
+			$content = array('chat_id' => $chat_id, 'text' => $reply);
+			$telegram->sendMessage($content);
+			exit;
+		} else {
+			switch ($adanParse[1]) {
+				case '--all':
+					$reply = '1. server-data.000webhostapp.com' . PHP_EOL .
+						'2. serv1-fadhil-riyanto-bot.herokuapp.com' . PHP_EOL .
+						'3. serv2-fadhil-riyanto-bot.herokuapp.com';
+					$content = array('chat_id' => $chat_id, 'text' => $reply);
+					$telegram->sendMessage($content);
+					break;
+
+				case '1':
+					$reply = file_get_contents('https://api.telegram.org/bot' . $telegramAPIs . '/setWebhook?url=https://server-data.000webhostapp.com');
+					$content = array('chat_id' => $chat_id, 'text' => $reply);
+					$telegram->sendMessage($content);
+					break;
+
+				case '2':
+					$reply = file_get_contents('https://api.telegram.org/bot' . $telegramAPIs . '/setWebhook?url=https://serv1-fadhil-riyanto-bot.herokuapp.com');
+					$content = array('chat_id' => $chat_id, 'text' => $reply);
+					$telegram->sendMessage($content);
+					break;
+
+				case '3':
+					$reply = file_get_contents('https://api.telegram.org/bot' . $telegramAPIs . '/setWebhook?url=https://serv2-fadhil-riyanto-bot.herokuapp.com');
+					$content = array('chat_id' => $chat_id, 'text' => $reply);
+					$telegram->sendMessage($content);
+					break;
+				default:
+					$reply = 'not found';
+					$content = array('chat_id' => $chat_id, 'text' => $reply);
+					$telegram->sendMessage($content);
+					break;
+			}
+		}
+	} else {
+		$reply = 'ACCES DENIED!, Hanya @fadhil_riyanto yang bisa mengexecute command ini!';
+		$content = array('chat_id' => $chat_id, 'text' => $reply);
+		$telegram->sendMessage($content);
+		exit;
+	}
+	exit;
 } elseif ('/ip_geo' == $adanParse[0] || '/ip_geo@fadhil_riyanto_bot' == $adanParse[0]) {
 	if ($adanParse[1] != null) {
 		//$getIPAPI = file_get_contents('https://ip-api.com/json/' . $adanParse[1]);
