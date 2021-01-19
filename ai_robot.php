fcfd16418fb7e958ed6fb337f9c8a92d8424f6f5
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-01-19 13:42:26 +0700

    update set ke global untuk koneksi MYSQL

--- ai_robot.php
+++ ai_robot.php
@@ -4,23 +4,8 @@ echo robot_artificial_intelegence('hai');
 function robot_artificial_intelegence($teks)
 {
 	require_once __DIR__ . '/vendor/autoload.php';
-	define('DB_HOST', 'freedb.tech');											//WAJIB
-	define('DB_USERNAME', 'freedbtech_ai_bot_fadhil_riyanto');					//WAJIB
-	define('DB_PASSWORD', '789b697698hyufijbbiub*&^BO&it87tbn7to&^7896');		//WAJIB
-	define('DB_NAME', 'freedbtech_ai_bot_fadhil_riyanto');						//WAJIB
-	define('TG_HTTP_API', '1489990155:AAEC3c6I-hmtDfbk9OojmlDjNFt1NeMEjfs');	//WAJIB
-	define('USER_ID_TG_ME', '1393342467');										//WAJIB
-	define('CUTLLY_API', 'fa1d93ba90dedd2ceb7d01e9bade271653373');				//WAJIB
-	define('TIME_ZONE', 'Asia/Jakarta');										//WAJIB
-	define('MAX_EXECUTE_SCRIPT', 20);											//SUNNAH_ROSUL
 
-	$koneksi = @mysqli_connect(
-		DB_HOST,
-		DB_USERNAME,
-		DB_PASSWORD,
-		DB_NAME
-	);
-
+	global $koneksi;
 	$stemmerFactory = new \Sastrawi\Stemmer\StemmerFactory();
 	$stemmer  = $stemmerFactory->createStemmer();
 
