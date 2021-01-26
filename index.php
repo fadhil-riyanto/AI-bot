087f3c5e86c601f94b0c6f0a9399217e6b1db2c2
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-01-26 19:02:30 +0700

    short

--- index.php
+++ index.php
@@ -1599,8 +1599,21 @@ elseif ('/faker' == $adanParse[0] || '/faker@fadhil_riyanto_bot' == $adanParse[0
 			$content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
 			$telegram->sendMessage($content);
 		} else {
-			$reply = $coronaprov; //jiika dia ngetik command doang tanpa parameter kirim ini
-			$content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
+			$reply = $coronaprov . PHP_EOL . PHP_EOL . 'Jangan lupa selalu pakai masker dan jaga kesehatan yaaa'; //jiika dia ngetik command doang tanpa parameter kirim ini
+			$option = array(
+				//First row
+				// array($telegram->buildInlineKeyBoardButton("Button 1", $url = "http://link1.com"), $telegram->buildInlineKeyBoardButton("Button 2", $url = "http://link2.com")),
+				// //Second row 
+				// array($telegram->buildInlineKeyBoardButton("Button 3", $url = "http://link3.com"), $telegram->buildInlineKeyBoardButton("Button 4", $url = "http://link4.com"), $telegram->buildInlineKeyBoardButton("Button 5", $url = "http://link5.com")),
+				// //Third row
+
+
+				array($telegram->buildInlineKeyBoardButton("covid19", $url = "https://covid19.go.id/"), $telegram->buildInlineKeyBoardButton("kemenkes", $url = "https://www.kemkes.go.id/"))
+			);
+			$keyb = $telegram->buildInlineKeyBoard($option);
+
+
+			$content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $keyb,  'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
 			$telegram->sendMessage($content);
 		}
 	} else {
