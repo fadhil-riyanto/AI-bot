22bb3ee5e9179124c6d9a1a47e6e8d9886313bea
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-01-27 21:07:14 +0700

    :v

--- test.php
+++ test.php
@@ -1,17 +1,10 @@
 <?php
-$udahDiparse = 'DKI Jakarta';
-$a_provinsi_corona = file_get_contents('https://api.kawalcorona.com/indonesia/provinsi/');
-$b_provinsi_corona = json_decode($a_provinsi_corona);
-foreach ($b_provinsi_corona as $bb_b_provinsi_corona) {
-    if ($bb_b_provinsi_corona->attributes->Provinsi == $udahDiparse) {
-        // echo $bb_b_provinsi_corona->attributes->Provinsi;
-        // echo $bb_b_provinsi_corona->attributes->Kode_Provi;
-        $reply = 'Provinsi : ' . $bb_b_provinsi_corona->attributes->Provinsi . PHP_EOL . PHP_EOL .
-            '😊 Total sembuh : ' . $bb_b_provinsi_corona->attributes->Kasus_Semb . PHP_EOL .
-            '😞 Total positif : ' . $bb_b_provinsi_corona->attributes->Kasus_Posi . PHP_EOL .
-            '😢 Total meninggal : ' . $bb_b_provinsi_corona->attributes->Kasus_Meni . PHP_EOL;
-    } else {
-        $reply = 'elses';
-    }
-}
-echo $reply;
+$teks = "hasil 8+7 dong";
+// INI INPUT
+
+$g  = preg_match('/[a-zA-Z]\s+([-+]?\s*\d+(?:\s*[-+*\/]\s*[-+]?\s*\d+)+)/i', $teks, $hasil);
+eval("\$h = $hasil[1];");
+$jawab = array("hasil adalah " . $h, "hyy hasil nya itu " . $h, "eh hasil nya " . $h);
+
+// INI OUTPUT
+echo $jawab[random_int(1, 2)];
