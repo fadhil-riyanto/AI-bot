75392861c9e2ef51191a1fd673e449d3155ad0a4
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-01-31 13:31:23 +0700

    ga kepakek

--- function/quran.php
+++ function/quran.php
@@ -2,18 +2,20 @@
 if (isset($_GET['surah'])) {
     $inputAyat = $_GET['surah'];
 }
+// <?php
+// $inputAyat = "al fatihaha 1";
 
 $preg1_surah = preg_match('/([a-zA-Z_+\- ]+)\s([-+]?\s*\d+(?:\s*)+)/i', $inputAyat, $hasilayat);
 if ($preg1_surah == 0) {
     preg_match('/([a-zA-Z_+\- ]+)/i', $inputAyat, $hasilayat);
 }
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://api.banghasan.com/quran/format/json/surat');
+curl_setopt($ch, CURLOPT_URL, 'https://api.npoint.io/99c279bb173a6e28359c/data');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $dataRawquran = curl_exec($ch);
 curl_close($ch);
 $jsonparseSurah = json_decode($dataRawquran);
-foreach ($jsonparseSurah->hasil as $quranfor) {
+foreach ($jsonparseSurah as $quranfor) {
     if (strtolower($quranfor->nama) == strtolower($hasilayat[1])) {
         $nomorSurah = $quranfor->nomor;
         $namaSurah = $quranfor->nama;
@@ -23,25 +25,22 @@ foreach ($jsonparseSurah->hasil as $quranfor) {
         $ayat_bools = true;
         if (isset($hasilayat[2])) {
             $ayatnum = true;
+        } else {
+            $ayatnum = false;
         }
         // return $nomorSurah;
     } else {
         $ayat_bool = true;
     }
 }
-if (isset($ayatnum) == true) {
-    $ayatjson = json_decode(file_get_contents('https://api.banghasan.com/quran/format/json/surat/' . $nomorSurah . '/ayat/' . $hasilayat[2]));
-    $reply =  $ayatjson->surat->nomor . '. ' . $ayatjson->surat->nama . ' / ' .
-        $ayatjson->surat->ayat . ' ayat, ' . $ayatjson->surat->type . PHP_EOL . 'ayat ke ' . $ayatjson->query->ayat . PHP_EOL . PHP_EOL .
-        $ayatjson->ayat->data->ar[0]->teks . PHP_EOL .
-        $ayatjson->ayat->data->idt[0]->teks . PHP_EOL .
-        $ayatjson->ayat->data->id[0]->teks . PHP_EOL;
-} elseif (isset($ayat_bools) == true) {
-    $reply = $nomorSurah . '. ' . $namaSurah .  PHP_EOL .
-        $ayatSurah . ' ayat, ' . $typeSurah .  PHP_EOL . PHP_EOL .
-        'keterangan : ' . $keteranganSurah . PHP_EOL;
-} elseif ($ayat_bool == true) {
-    $reply = 'gk nemu';
+if (@$ayatnum == true) {
+    $ayatjson = json_decode(file_get_contents('https://api.npoint.io/99c279bb173a6e28359c/surat/' . $nomorSurah));
+    foreach ($ayatjson as $ayatjsons) {
+        if ($ayatjsons->nomor == $hasilayat[2]) {
+            echo $ayatjsons->ar . PHP_EOL;
+            echo $ayatjsons->id;
+        }
+    }
+} elseif (@$ayat_bools == true) {
+    echo 'ga';
 }
-header('type: text/plain');
-echo json_encode(array('hasil' => $reply));
