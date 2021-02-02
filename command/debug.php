7be9cf9d6bbadceb43565d71f782ea95d0dc33a4
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-02-02 21:37:58 +0700

    njajal

--- command/debug.php
+++ command/debug.php
@@ -1,12 +1,12 @@
 <?php
-
-$reply = "tes";
 $option = array(
-    array(
-        $telegram->buildInlineKeyBoardButton("tombol HAHAHA", $url = "", $callback_data = '/callback_q@fadhil_riyanto_bot panggil'),
-        $telegram->buildInlineKeyBoardButton("dua", $url = "", $callback_data = '/callback_q@fadhil_riyanto_bot start')
-    ),
+    //First row
+    array($telegram->buildInlineKeyBoardButton("Button 1", $url = "http://link1.com"), $telegram->buildInlineKeyBoardButton("Button 2", $url = "http://link2.com")),
+    //Second row 
+    array($telegram->buildInlineKeyBoardButton("base64_decode", $url = "http://link3.com"), $telegram->buildInlineKeyBoardButton("get_github_user", $url = "http://link4.com"), $telegram->buildInlineKeyBoardButton("base64_decode", $url = "http://link5.com")),
+    //Third row
+    array($telegram->buildInlineKeyBoardButton("Button 6", $url = "http://link6.com"))
 );
 $keyb = $telegram->buildInlineKeyBoard($option);
-$content = array('chat_id' => $chat_id, 'text' => $reply,  'reply_markup' => $keyb, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
+$content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "This is a Keyboard Test");
 $telegram->sendMessage($content);
