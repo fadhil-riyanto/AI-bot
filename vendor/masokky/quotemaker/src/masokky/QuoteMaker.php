d47299f75cdff4187f0b85d9663f7f45d07410ed
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-02-15 15:38:13 +0700

    fix image resize

--- vendor/masokky/quotemaker/src/masokky/QuoteMaker.php
+++ vendor/masokky/quotemaker/src/masokky/QuoteMaker.php
@@ -264,7 +264,7 @@ class QuoteMaker {
 	}
 	private function create_template(){
 		self::$image
-				->resize(1000,600)
+				->resize(1280,720)
 				->darken(20)
 				->overlay(self::$top_quote_mark,"center",1,0,-250)
 				->overlay(self::$bottom_quote_mark,"center",1,0,100);
