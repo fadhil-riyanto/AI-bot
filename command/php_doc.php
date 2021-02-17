51bd90df43e0ba55cbe40468229c8ee79dc59569
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-02-17 12:39:20 +0700

    add phpdocs

--- command/php_doc.php
+++ command/php_doc.php
@@ -20,48 +20,39 @@ function removeEscapedphpdocs($string)
         $string
     );
 }
+function removespasikelebihan($str)
+{
+    $ro = preg_replace('/\s+/', ' ', $str);
+    return $ro;
+}
 if ($azanHilangcommand == null) {
     $reply = 'Hai ' . $username . PHP_EOL . 'Untuk menggunakan phpdocs, gunakan command <pre>/php_docs {teks atau kata}</pre>' . PHP_EOL . PHP_EOL .
         'Contoh : <pre>/quotes kamu bagaikan bunga dipagi hari</pre>';
     $content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
     $telegram->sendMessage($content);
 } else {
-    $html = file_get_html('https://www.php.net/' . $udahDiparse);
 
-    foreach ($html->find('div[class=methodsynopsis dc-description]') as $elements) {
-        if ($elements->plaintext == null) {
-            echo "ga nemu";
-            exit;
-        }
-    }
-    foreach ($html->find('h1[class=refname]') as $elements)
-        $phpdoc1 = "<b>" . $elements->plaintext . "</b>" . PHP_EOL;
-
-    foreach ($html->find('p[class=verinfo]') as $elements)
-        $phpdoc2 =  "<i>" . $elements->plaintext . "</i>" . PHP_EOL . PHP_EOL;
-
-    foreach ($html->find('p[class=refpurpose]') as $elements)
-        $phpdoc3 =  "<i>" . $elements->plaintext . "</i>" . PHP_EOL . PHP_EOL;
+    $funcname = $udahDiparse;
+    $html = file_get_html('https://www.php.net/' . $funcname);
 
     foreach ($html->find('div[class=methodsynopsis dc-description]') as $elements)
-        $phpdoc4 = '<b>parameters</b> : <pre>' . $elements->plaintext . "</pre>" . PHP_EOL . PHP_EOL;
+        $parameterswkwk = '<b>parameters</b>' . PHP_EOL . '<pre>' . removespasikelebihan($elements->plaintext) . "</pre>";
 
-    foreach ($html->find('p[class=para rdfs-comment]') as $elements)
-        $phpdoc5 = '<b>Description : </b>' . $elements->plaintext;
+    $phpdocfiles = file_get_contents(__DIR__ . "/../json_data/phpdocs/database.json");
+    $phpdoc = json_decode($phpdocfiles);
 
-    foreach ($html->find('div[class=caution]') as $elements)
-        $phpdoc6 = $elements->plaintext;
+    $hasil = '<b>' . $phpdoc->$funcname->name . '</b>' . PHP_EOL .
+        '<i>' . $phpdoc->$funcname->ver . '</i>' . PHP_EOL . PHP_EOL .
+        '<pre>' . $phpdoc->$funcname->name . '</pre> - ' . $phpdoc->$funcname->desc . PHP_EOL . PHP_EOL .
+        $parameterswkwk  . PHP_EOL . PHP_EOL .
+        '<b>Description</b>' . PHP_EOL . $phpdoc->$funcname->long_desc . PHP_EOL . PHP_EOL .
+        '<b>Return value</b>' . PHP_EOL . $phpdoc->$funcname->ret_desc;
 
-    foreach ($html->find('div[class=refsect1 returnvalues]') as $elements)
-        $phpdoc7 =   $elements->plaintext;
-
-    if (isset($phpdoc6)) {
-        $reply =  $phpdoc1 . $phpdoc2 . $phpdoc3 . $phpdoc4 . $phpdoc5 . $phpdoc6 . PHP_EOL . PHP_EOL . $phpdoc7;
-        $content = array('chat_id' => $chat_id, 'text' => removeEscapedphpdocs($reply), 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
-        $telegram->sendMessage($content);
-    } else {
-        $reply =  $phpdoc1 . $phpdoc2 . $phpdoc3 . $phpdoc4 . $phpdoc5 . PHP_EOL . PHP_EOL . $phpdoc7;
-        $content = array('chat_id' => $chat_id, 'text' => removeEscapedphpdocs($reply), 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
-        $telegram->sendMessage($content);
-    }
+    $option = array(
+        array($telegram->buildInlineKeyBoardButton("Ducumentation", $url = "https://www.php.net/manual/en/" . $phpdoc->$funcname->url))
+    );
+    $keyb = $telegram->buildInlineKeyBoard($option);
+    $reply = removeEscapedphpdocs($hasil);
+    $content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $keyb, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
+    $telegram->sendMessage($content);
 }
