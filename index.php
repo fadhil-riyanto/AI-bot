59d3bfe352a60a5a7292e039e15c929c8d22f2f6
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-02-18 13:49:29 +0700

    pakek komepage

--- index.php
+++ index.php
@@ -1,27 +1,36 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 <head>
-  <title>Hello, world!</title>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<meta name="description" content="Closed API for forever">
+<meta name="keywords" content="">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Meta Finish -->
+
+
+  <meta charset="utf-8"/>
+  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport'/>
   <!--     Fonts and icons     -->
-  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
-  <!-- Material Kit CSS -->
-  <link href="assets/css/material-kit.css?v=2.0.7" rel="stylesheet" />
+  <link rel="stylesheet" type="text/css" href="assets/css/fonts.css"/>
+  <link rel="stylesheet" href="assets/css/fontawesome.css">
+  <!-- CSS Files -->
+  <link href="assets/css/material-kit.css?v=2.0.3" rel="stylesheet"/>
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="assets/demo/demo.css" rel="stylesheet"/>
 </head>
 
-<body>
-  <nav class="navbar navbar-color-on-scroll navbar-transparent fixed-top navbar-expand-lg" color-on-scroll="100">
+<body class="index-page sidebar-collapse">
+  <nav class="navbar navbar-transparent navbar-color-on-scroll fixed-top navbar-expand-lg" color-on-scroll="100" id="sectionsNav">
     <div class="container">
       <div class="navbar-translate">
-        <a class="navbar-brand" href="https://demos.creative-tim.com/material-kit/index.html">
-          Material Kit </a>
+        <a class="navbar-brand" href="https://www.developers.eu.org/">
+          Fadhil's APIs </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="sr-only">Toggle navigation</span>
           <span class="navbar-toggler-icon"></span>
           <span class="navbar-toggler-icon"></span>
           <span class="navbar-toggler-icon"></span>
@@ -29,54 +38,297 @@
       </div>
       <div class="collapse navbar-collapse">
         <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="material-icons">apps</i> Template
+          <li class="dropdown nav-item">
+            <a href="https://github.com/fadhil-riyanto" class=" nav-link" >
+              <i class="material-icons">settings_input_component</i> Github
+            </a>
+            
+          </li>
+          <li class="dropdown nav-item">
+           <a href="https://www.developers.eu.org/" class=" nav-link" >
+              <i class="material-icons">settings_input_component</i> official website
             </a>
           </li>
+          
         </ul>
       </div>
     </div>
   </nav>
-  <div class="page-header header-filter" data-parallax="true" style="background-image: url('assets/img/img16-min.jpg')">
+  <div class="page-header header-filter clear-filter" data-parallax="true" style="background-image: url('assets/img/pexels-photo-758742-min.jpg');">
     <div class="container">
       <div class="row">
         <div class="col-md-8 ml-auto mr-auto">
-          <div class="brand text-center">
-            <h1>Your title here</h1>
-            <h3 class="title text-center">Subtitle</h3>
+          <div class="brand">
+            <h1>Fadhil's APIs</h1>
+            <h3>Only god can stop me | Belajar apapun yg aku suka dan tanpa paksaan.</h3>
           </div>
         </div>
       </div>
+      <br/><div class="alert alert-success">
+        <div class="container">
+          <div class="alert-icon">
+            <i class="material-icons">check</i>
+          </div>
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true"><i class="material-icons">clear</i></span>
+          </button>
+          <strong style="color:red">WAJIB!!</strong> untuk mendapatkan apikey hubungi pemilik api ini, 
+        </div>
+      </div>
     </div>
-  </div>
+  </div><!-- Meta Start -->
+
+<!-- Meta Finish -->
+
+<title>Fadhil's - APIs</title>
   <div class="main main-raised">
-    <div class="container">
-      <div class="section text-center">
-        <h2 class="title">Your main section here</h2>
+    <div class="section section-basic">
+      <div class="container">
+			<!-- Content -->
+        <div id="nav-tabs">
+          <div class="row">
+            <div class="col-md-12">
+              <!-- Tabs with icons on Card -->
+              <div class="card card-nav-tabs">
+                <div class="card-header card-header-primary">
+                  <!-- colors: "header-primary", "header-info", "header-success", "header-warning", "header-danger" -->
+                  <div class="nav-tabs-navigation">
+                    <div class="nav-tabs-wrapper">
+                      <ul class="nav nav-tabs" data-tabs="tabs">
+                        <li class="nav-item">
+                          <a class="nav-link active" href="#param" data-toggle="tab">
+                            <i class="material-icons">face</i> URL + Paramaters
+                          </a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#example" data-toggle="tab">
+                            <i class="material-icons">settings</i> #1 Example
+                          </a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#example2" data-toggle="tab">
+                            <i class="material-icons">settings</i> #2 Example
+                          </a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#example3" data-toggle="tab">
+                            <i class="material-icons">settings</i> #3 Example
+                          </a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#example4" data-toggle="tab">
+                            <i class="material-icons">settings</i> #4 Example
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div class="card-body ">
+                  <div class="tab-content">
+                    <div class="tab-pane active" id="param">
+                            <br/>Source: Planetbiru.com<br/><br/><div class="blockquote undefined"><span class="badge badge-primary">GET [SSL]</span> <font size="3">https://rest.farzain.com/api/ramal_XXXXXX.php</font><br><br/><span class="badge badge-primary">GET [NO SSL]</span> <font size="3">http://api.farzain.com/ramal_XXXXXX.php</font><br></div><br/>
+                            change ramal_XXXXXX to the correct url, here's a list of usable urls<br/>
+                            ramal_nama, ramal_hobi, ramal_fans, ramal_keluarga<br/><br/>
+                            <table class="table table-bordered">
+                                <tbody><tr>
+                                    <th>Parameter</th>
+                                    <th>Required</th>
+                                    <th>Description</th>
+                                </tr>
+                                <tr>
+                                    <td>1.) apikey</td>
+                                    <td><i>yes</i></td>
+                                    <td>your apikey <a href="https://farzan.com/user/signup.php">click here</a> to get your free apikey</td>
+                                </tr>
+                                <tr>
+                                    <td>2.) name</td>
+                                    <td><i>yes</i></td>
+                                    <td>Your name</td>
+                                </tr>
+                                <tr>
+                                    <td>3.) name2</td>
+                                    <td><i>no</i></td>
+                                    <td>if you use ramal_keluarga.php</td>
+                                </tr>
+                            </tbody></table>
+                    </div>
+                    <div class="tab-pane" id="example">
+                      <br/>Source: Planetbiru.com<br/><br/><div class="blockquote undefined"><span class="badge badge-primary">GET [SSL]</span> <font size="3">https://rest.farzain.com/api/ramal_nama.php?apikey=beta&name=udin</font><br><br/><span class="badge badge-primary">GET [NO SSL]</span> <font size="3">http://api.farzain.com/ramal_nama.php?apikey=beta&name=udin</font><br></div><br/>
+                        <xmp style="padding:10px;background:#263238;color:#fff;min-height:300px">
+{
+    "status": 200,
+    "creator": "Faraaz",
+    "result": [
+        {
+            "nama": "ROMANTIS",
+            "hasil": "26%"
+        },
+        {
+            "nama": "MESUM",
+            "hasil": "47%"
+        },
+        {
+            "nama": "MIRIS",
+            "hasil": "69%"
+        },
+        {
+            "nama": "TULUS",
+            "hasil": "63%"
+        },
+        {
+            "nama": "LOYAL",
+            "hasil": "31%"
+        }
+    ]
+}                        </xmp>
+                    </div>
+                    <div class="tab-pane" id="example2">
+                      <br/>Source: Planetbiru.com<br/><br/><div class="blockquote undefined"><span class="badge badge-primary">GET [SSL]</span> <font size="3">https://rest.farzain.com/api/ramal_hobi.php?apikey=beta&name=udin</font><br><br/><span class="badge badge-primary">GET [NO SSL]</span> <font size="3">http://api.farzain.com/ramal_hobi.php?apikey=beta&name=udin</font><br></div><br/>
+                        <xmp style="padding:10px;background:#263238;color:#fff;min-height:300px">
+{
+    "status": 200,
+    "creator": "Faraaz",
+    "result": [
+        {
+            "nama": "Makanan",
+            "hasil": "Nasi Kucing"
+        },
+        {
+            "nama": "Minuman",
+            "hasil": "Bandreks"
+        },
+        {
+            "nama": "Kebiasaan Baik",
+            "hasil": "Merapikan Kamar"
+        },
+        {
+            "nama": "Kebiasaan Buruk",
+            "hasil": "Menguap"
+        }
+    ]
+}                        </xmp>
+                    </div>
+                    <div class="tab-pane" id="example3">
+                      <br/>Source: Planetbiru.com<br/><br/><div class="blockquote undefined"><span class="badge badge-primary">GET [SSL]</span> <font size="3">https://rest.farzain.com/api/ramal_fans.php?apikey=beta&name=udin</font><br><br/><span class="badge badge-primary">GET [NO SSL]</span> <font size="3">http://api.farzain.com/ramal_fans.php?apikey=beta&name=udin</font><br></div><br/>
+                        <xmp style="padding:10px;background:#263238;color:#fff;min-height:300px">
+{
+    "status": 200,
+    "creator": "Faraaz",
+    "result": [
+        {
+            "nama": "PENGAGUM RAHASIA",
+            "hasil": "3 orang"
+        },
+        {
+            "nama": "CURI CURI PANDANG",
+            "hasil": "9 orang"
+        },
+        {
+            "nama": "DIAM DIAM KEPO",
+            "hasil": "5 orang"
+        },
+        {
+            "nama": "DIAM DIAM PERHATIAN",
+            "hasil": "20 orang"
+        },
+        {
+            "nama": "DIAM DIAM SUKA",
+            "hasil": "17 orang"
+        }
+    ]
+}                        </xmp>
+                    </div>
+                    <div class="tab-pane" id="example4">
+                      <br/>Source: Planetbiru.com<br/><br/><div class="blockquote undefined"><span class="badge badge-primary">GET [SSL]</span> <font size="3">https://rest.farzain.com/api/ramal_keluarga.php?apikey=beta&name=udin&nama2=juminten</font><br><br/><span class="badge badge-primary">GET [NO SSL]</span> <font size="3">http://api.farzain.com/ramal_keluarga.php?apikey=beta&name=udin&nama2=juminten</font><br></div><br/>
+                        <xmp style="padding:10px;background:#263238;color:#fff;min-height:300px">
+{
+    "status": 200,
+    "creator": "Faraaz",
+    "result": [
+        "Jumlah Anak Maksimum: 12 Orang",
+        "Jarak Kelahiran Anak Minimum: 9 Bulan",
+        "Kemungkinan Anak Pertama: Laki-Laki",
+        "Kemungkinan Poligami: 70%",
+        "Kemungkinan Cerai: 57%",
+        "Kemungkinan Selingkuh: 40%",
+        "Kemungkinan Bertengkar: 28 Kali Setahun"
+    ]
+}                        </xmp>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- End Tabs with icons on Card -->
+            </div>
+          </div>
+        </div>
+			<!-- Content -->
+        <div class="space-50"></div>
+
       </div>
     </div>
   </div>
-  <footer class="footer footer-default">
+  <footer class="footer" data-background-color="black">
     <div class="container">
-      <nav class="float-left">
-        <ul>
-          <li>
-            <a href="https://www.creative-tim.com/">
-              Creative Tim
-            </a>
-          </li>
-        </ul>
-      </nav>
+      
       <div class="copyright float-right">
         &copy;
         <script>
           document.write(new Date().getFullYear())
-        </script>, made with <i class="material-icons">favorite</i> by
-        <a href="https://www.creative-tim.com/" target="blank">Creative Tim</a> for a better web.
+        </script>, Fadhil Riyanto.
       </div>
     </div>
-  </footer>
-</body>
+  </footer>  <!--<div id="SC_TBlock_524464" class="SC_TBlock">loading...</div> -->
+  <!--   Core JS Files   -->
+<script>
+function myFunction() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+</script>
 
+
+  <script src="assets/js/core/jquery.min.js" type="text/javascript"></script>
+  <script src="assets/js/core/popper.min.js" type="text/javascript"></script>
+  <script src="assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
+  <script src="assets/js/plugins/moment.min.js"></script>
+  <!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
+  <script src="assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
+  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+  <script src="assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+  <!-- Control Center for Now Ui Kit: parallax effects, scripts for the example pages etc -->
+  <script src="assets/js/material-kite8da.js?v=2.0.3" type="text/javascript"></script>
+  <script>
+    $(document).ready(function() {
+      //init DateTimePickers
+      materialKit.initFormExtendedDatetimepickers();
+
+      // Sliders Init
+      materialKit.initSliders();
+    });
+
+
+    function scrollToDownload() {
+      if ($('.section-download').length != 0) {
+        $("html, body").animate({
+          scrollTop: $('.section-download').offset().top
+        }, 1000);
+      }
+    }
+  </script>
+</body>
 </html>
