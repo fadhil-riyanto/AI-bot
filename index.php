873c173bf15bb447a2f61a08413348896071ecc3
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-02-18 09:12:53 +0700

    ubah latar belakang

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
       </div>
     </div>
   </nav>
-  <div class="page-header header-filter" data-parallax="true" style="background-image: url('assets/img/bg3.jpg')">
+  <div class="page-header header-filter" data-parallax="true" style="background-image: url('assets/img/img16-min.jpg')">
     <div class="container">
       <div class="row">
         <div class="col-md-8 ml-auto mr-auto">
