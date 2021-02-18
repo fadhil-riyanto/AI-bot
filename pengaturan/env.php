6cc04994e179bb79a38bbeea5da67b1cb7592ca7
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-02-18 13:51:53 +0700

    adding konstatta (coreutil)

---
+++ pengaturan/env.php
@@ -0,0 +1,20 @@
+<?php
+define('BOT_VERSION', '10.0.2'); //informasi versi bot
+define('DB_HOST', 'freedb.tech');											//WAJIB
+define('DB_USERNAME', 'freedbtech_ai_bot_fadhil_riyanto');					//WAJIB
+define('DB_PASSWORD', '789b697698hyufijbbiub*&^BO&it87tbn7to&^7896');		//WAJIB
+define('DB_NAME', 'freedbtech_ai_bot_fadhil_riyanto');						//WAJIB
+define('TG_HTTP_API', '1489990155:AAHJ4-6PcvnNtvZszUeYrfKsAcICxVAfb6A');	//WAJIB
+define('USER_ID_TG_ME', '1393342467');										//WAJIB
+define('CUTLLY_API', 'fa1d93ba90dedd2ceb7d01e9bade271653373');				//WAJIB
+define('TIME_ZONE', 'Asia/Jakarta');										//WAJIB
+define('API_WEATHER_KEY', '7cf7252c68d3473681054158212501');				//WAJIB
+define('ID_BOT', '1489990155');	                                  			//WAJIB
+define('MAX_EXECUTE_SCRIPT', 5000);											//SUNNAH_ROSUL
+define('USERNAME_BOT', '@fadhil_riyanto_bot');								// pakai at@
+define('PUMBUAT_BOT', '@fadhil_riyanto');								// pakai at@
+define('SUPPORT_GROUP', 'https://t.me/tgdev_php_group');				// type url
+define('VERSI_PHP', phpversion());				// type url
+define('HOSTED_BY', 'Heroku');				// type url
+define('DATABASE_PLATFORM', 'Mysql');				// type url
+define('SERVER_HTTP_PLATFORM', 'nginx');				// type url
