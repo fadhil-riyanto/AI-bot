a18e5998e3a062cccd0082b0334b255a577e81ee
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-02-19 22:10:54 +0700

    debug

--- command/debug.php
+++ command/debug.php
@@ -1,10 +1,30 @@
 <?php
-$content = array('chat_id' => $chat_id);
-$adminlist = $telegram->getChatAdministrators($content);
-foreach ($adminlist['result'] as $daftar) {
-    $data[] = $daftar['user']['id'];
+$db = new MysqliDb(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$db->where("gid", $chat_id);
+$user = $db->getOne("grup_data");
+if ($user['welcome_text'] == null) {
+    $reply = 'Halo, apa kabar mu?';
+    $content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
+    $telegram->sendMessage($content);
+} else {
+    function parsewelcome($string)
+    {
+        global $username;
+        $dict = array(
+            '[[username]]' => $username,
+            'yoi' => 'iya'
+
+            // replace teks lainnya disini
+        );
+        return strtolower(
+            str_replace(
+                array_keys($dict),
+                array_values($dict),
+                $string
+            )
+        );
+    }
+    $reply = parsewelcome($user['welcome_text']);
+    $content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
+    $telegram->sendMessage($content);
 }
-//g jadi lah
-$reply = echo $data[0];
-$content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => false);
-$urlCall = $telegram->sendMessage($content);
