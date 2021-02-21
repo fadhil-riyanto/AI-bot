3c3f3869831f467cb2d5c7f87b88bb10170a41c8
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-02-21 15:20:34 +0700

    perbaikan azan dan add help menu (blum fly update)

--- admin_command/__init__.php
+++ admin_command/__init__.php
@@ -97,7 +97,6 @@ if (deteksi_grup() == true) {
         $content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
         $telegram->sendMessage($content);
     }
-
     exit;
 }
 
