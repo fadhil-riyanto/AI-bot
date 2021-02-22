ed7c41c339792393424891e9858377633b5e191f
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-02-22 13:45:17 +0700

    \

--- command/tanggal.php
+++ command/tanggal.php
@@ -1,4 +1,40 @@
 <?php
-$reply = 'Hai ' . $username . PHP_EOL . PHP_EOL . 'sekarang tanggal ' . date('l, d F Y');
-	$content = array('chat_id' => $chat_id, 'text' => $reply,  'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
-	$telegram->sendMessage($content);
+function tanggal_indo($tanggal, $cetak_hari = false)
+{
+	$hari = array(
+		1 =>    'Senin',
+		'Selasa',
+		'Rabu',
+		'Kamis',
+		'Jumat',
+		'Sabtu',
+		'Minggu'
+	);
+
+	$bulan = array(
+		1 =>   'Januari',
+		'Februari',
+		'Maret',
+		'April',
+		'Mei',
+		'Juni',
+		'Juli',
+		'Agustus',
+		'September',
+		'Oktober',
+		'November',
+		'Desember'
+	);
+	$split 	  = explode('-', $tanggal);
+	$tgl_indo = $split[2] . ' ' . $bulan[(int)$split[1]] . ' ' . $split[0];
+
+	if ($cetak_hari) {
+		$num = date('N', strtotime($tanggal));
+		return $hari[$num] . ', ' . $tgl_indo;
+	}
+	return $tgl_indo;
+}
+
+$reply = 'Hai ' . $username . PHP_EOL . PHP_EOL . 'sekarang tanggal ' . tanggal_indo(date('Y-m-d'));
+$content = array('chat_id' => $chat_id, 'text' => $reply,  'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
+$telegram->sendMessage($content);
