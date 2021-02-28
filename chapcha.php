06a449f55cc975954f17bcb0c52c873bc190b6b3
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-02-28 21:02:22 +0700

    test gen chapcha

--- chapcha.php
+++ chapcha.php
@@ -2,8 +2,12 @@
 require 'vendor/autoload.php';
 
 use Gregwar\Captcha\CaptchaBuilder;
+use Gregwar\Captcha\PhraseBuilder;
 
-$builder = new CaptchaBuilder;
+$phraseBuilder = new PhraseBuilder(4, '0123456789');
+
+
+$builder = new CaptchaBuilder(null, $phraseBuilder);
 $builder->build($width = 300, $height = 80, $font = null);
 echo "<img src=" . $builder->inline() . " />";
 echo $builder->getPhrase();
--- command/debug.php
+++ command/debug.php
@@ -1,30 +1,49 @@
 <?php
-$db = new MysqliDb(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
-$db->where("gid", $chat_id);
-$user = $db->getOne("grup_data");
-if ($user['welcome_text'] == null) {
-    $reply = 'Halo, apa kabar mu?';
-    $content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
-    $telegram->sendMessage($content);
-} else {
-    function parsewelcome($string)
-    {
-        global $username;
-        $dict = array(
-            '[[username]]' => $username,
-            'yoi' => 'iya'
-
-            // replace teks lainnya disini
-        );
-        return strtolower(
-            str_replace(
-                array_keys($dict),
-                array_values($dict),
-                $string
-            )
-        );
-    }
-    $reply = parsewelcome($user['welcome_text']);
-    $content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
-    $telegram->sendMessage($content);
+function random_angka_digit($digits)
+{
+    return rand(pow(10, $digits - 1), pow(10, $digits) - 1);
 }
+
+$keylist_chapcha_atas0 = array(
+    $telegram->buildInlineKeyBoardButton(random_angka_digit(4), $url = "",  $callback_data = "/help"),
+    $telegram->buildInlineKeyBoardButton(random_angka_digit(4), $url = "",  $callback_data = "/help"),
+    $telegram->buildInlineKeyBoardButton('ini', $url = "",  $callback_data = "/help"),
+    $telegram->buildInlineKeyBoardButton(random_angka_digit(4), $url = "",  $callback_data = "/help"),
+    $telegram->buildInlineKeyBoardButton(random_angka_digit(4), $url = "",  $callback_data = "/help")
+);
+$keylist_chapcha_atas1 = array(
+    $telegram->buildInlineKeyBoardButton(random_angka_digit(4), $url = "",  $callback_data = "/help"),
+    $telegram->buildInlineKeyBoardButton(random_angka_digit(4), $url = "",  $callback_data = "/help"),
+    $telegram->buildInlineKeyBoardButton(random_angka_digit(4), $url = "",  $callback_data = "/help"),
+    $telegram->buildInlineKeyBoardButton(random_angka_digit(4), $url = "",  $callback_data = "/help"),
+    $telegram->buildInlineKeyBoardButton(random_angka_digit(4), $url = "",  $callback_data = "/help")
+);
+
+$keylist_chapcha_bawah0 = array(
+    $telegram->buildInlineKeyBoardButton(random_angka_digit(4), $url = "",  $callback_data = "/help"),
+    $telegram->buildInlineKeyBoardButton(random_angka_digit(4), $url = "",  $callback_data = "/help"),
+    $telegram->buildInlineKeyBoardButton(random_angka_digit(4), $url = "",  $callback_data = "/help"),
+    $telegram->buildInlineKeyBoardButton(random_angka_digit(4), $url = "",  $callback_data = "/help"),
+    $telegram->buildInlineKeyBoardButton(random_angka_digit(4), $url = "",  $callback_data = "/help")
+);
+$keylist_chapcha_bawah1 = array(
+    $telegram->buildInlineKeyBoardButton(random_angka_digit(4), $url = "",  $callback_data = "/help"),
+    $telegram->buildInlineKeyBoardButton(random_angka_digit(4), $url = "",  $callback_data = "/help"),
+    $telegram->buildInlineKeyBoardButton('ini', $url = "",  $callback_data = "/help"),
+    $telegram->buildInlineKeyBoardButton(random_angka_digit(4), $url = "",  $callback_data = "/help"),
+    $telegram->buildInlineKeyBoardButton(random_angka_digit(4), $url = "",  $callback_data = "/help")
+);
+//bagian atas
+shuffle($keylist_chapcha_atas0);
+shuffle($keylist_chapcha_atas1);
+$option0 = array($keylist_chapcha_atas0, $keylist_chapcha_atas1);
+
+//bagian bawah
+shuffle($keylist_chapcha_bawah0);
+shuffle($keylist_chapcha_bawah1);
+$option1 = array($keylist_chapcha_bawah0, $keylist_chapcha_bawah1);
+
+$randomatasbawah = array($option0, $option1);
+$keyb = $telegram->buildInlineKeyBoard($randomatasbawah[random_int(0, 1)]);
+$content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "This is a Keyboard Test");
+$telegram->sendMessage($content);
