ffbf86a47ddf10a37ca124e6780419cb4f953c81
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-03-14 20:17:34 +0700

    update list

--- hook.php
+++ hook.php
@@ -249,7 +249,7 @@ if (isset($text)) {
 	$chek_gc = detect_grup();
 	$nama_gc = $chat_id;
 	if ($chek_gc == true) {
-		if ($nama_gc == -1001209274058 || $nama_gc == -1001410961692 || $nama_gc == -458987087 || $nama_gc == -1001433395819) {
+		if ($nama_gc == -1001410961692 || $nama_gc == -458987087 || $nama_gc == -1001433395819) {
 		} else {
 			$reply = 'Maaf, saya diprogram oleh pemilik saya untuk tidak dimasukkan ke grup secara sembarangan oleh orang' . PHP_EOL . PHP_EOL .
 				'Jika anda masih tetap ingin memasukkan saya ke grup. silahkan copy angka ini <pre>' . $chat_id . '</pre> lalu pm atau kirimkan ke pembuat bot ini, yaitu ' . PUMBUAT_BOT . PHP_EOL . PHP_EOL .
