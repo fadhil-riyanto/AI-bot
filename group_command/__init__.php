5883f4c35e3393c3d5c92b818e129b48a916e88d
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-03-19 23:19:19 +0700

    njajal

--- group_command/__init__.php
+++ group_command/__init__.php
@@ -1,12 +1,17 @@
 <?php
 if ($adanParse[0] == '/afk' ||  '/afk' . USERNAME_BOT . '' == $adanParse[0]) {
     require 'afk.php';
+    exit;
 } elseif ($adanParse[0] == '/unafk' || '/unafk' . USERNAME_BOT . '' == $adanParse[0]) {
     require 'unafk.php';
+    exit;
 } elseif ($adanParse[0] == '/invitelink' || '/invitelink' . USERNAME_BOT . '' == $adanParse[0]) {
     require 'invitelink.php';
+    exit;
 } elseif ($adanParse[0] == '/adminlist' || '/adminlist' . USERNAME_BOT . '' == $adanParse[0]) {
     require 'invitelink.php';
+    exit;
 } elseif ($adanParse[0] == '/report' || $adanParse[0] == '/r' || '/report' . USERNAME_BOT . '' == $adanParse[0]) {
     require 'report.php';
+    exit;
 }
