2bd66c2154cc2df69f160537a827e3ad68f32bcc
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-03-21 14:23:19 +0700

    update afk system

--- include/autounafk.php
+++ include/autounafk.php
@@ -9,7 +9,7 @@ function deteksi_grupP()
 }
 if (deteksi_grupP() == true) {
     $delaycached = 20;
-    $getlistjsonadmin = __DIR__ . '/../json_data/afkstatus.json';
+    $getlistjsonadmin = __DIR__ . '/json_data/afkstatus.json';
     function GETuseridafk($userID)
     {
         global $getlistjsonadmin, $timed, $alasan, $uid_afk;
@@ -33,7 +33,7 @@ if (deteksi_grupP() == true) {
         $content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
         $telegram->sendMessage($content);
 
-        $file = __DIR__ . '/../json_data/afkstatus.json';
+        $file = __DIR__ . '/json_data/afkstatus.json';
         $anggota = file_get_contents($file);
         $data = json_decode($anggota, true);
 
@@ -51,51 +51,51 @@ if (deteksi_grupP() == true) {
         $user = $db->getOne("afk_user_data");
 
         $db->delete('afk_user_data');
-        exit;
-    }
-    if ($cekher == true) {
-        $reply = "Maaf ," . $afkforstname . ' ' . $afklastname . ' sedang AFK sejak ' . $timed . PHP_EOL .
-            'Alasan : ' . $alasan;
-        $content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
-        $telegram->sendMessage($content);
-    } elseif ($cekher == false) {
-        if (isset($getreplyianid)) {
-            $db = new MysqliDb(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
-            $db->where("userid", $getreplyianid);
-            $user = $db->getOne("afk_user_data");
-            if ($user['userid'] == null) {
-            } else {
-                if ($getreplyianid == $userID) {
-                    if (substr($text_plain_nokarakter, 0, 1) == '/') {
+    } else {
+        if ($cekher == true) {
+            $reply = "Maaf ," . $afkforstname . ' ' . $afklastname . ' sedang AFK sejak ' . $timed . PHP_EOL .
+                'Alasan : ' . $alasan;
+            $content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
+            $telegram->sendMessage($content);
+        } elseif ($cekher == false) {
+            if (isset($getreplyianid)) {
+                $db = new MysqliDb(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+                $db->where("userid", $getreplyianid);
+                $user = $db->getOne("afk_user_data");
+                if ($user['userid'] == null) {
+                } else {
+                    if ($getreplyianid == $userID) {
+                        if (substr($text_plain_nokarakter, 0, 1) == '/') {
+                        } else {
+                            exit;
+                        }
                     } else {
+                        //disini update dan edit json
+
+                        // Mengencode data menjadi json
+                        $file = __DIR__ . '/json_data/afkstatus.json';
+                        $anggota = file_get_contents($file);
+                        $data = json_decode($anggota, true);
+
+                        $data[] = array(
+                            'userid'     => $user['userid'],
+                            'time_afk'   => $user['time_afk'],
+                            'alasan' => $user['alasan'],
+                            'username' => $user['username']
+                        );
+
+                        $jsonfile = json_encode($data, JSON_PRETTY_PRINT);
+                        $anggota = file_put_contents($file, $jsonfile);
+
+                        $reply = "Maaf ," . $afkforstname . ' ' . $afklastname . ' sedang AFK sejak ' . $user['time_afk'] . PHP_EOL .
+                            'Alasan : ' . $user['alasan'];
+                        $content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
+                        $telegram->sendMessage($content);
                         exit;
                     }
-                } else {
-                    //disini update dan edit json
-
-                    // Mengencode data menjadi json
-                    $file = __DIR__ . '/../json_data/afkstatus.json';
-                    $anggota = file_get_contents($file);
-                    $data = json_decode($anggota, true);
-
-                    $data[] = array(
-                        'userid'     => $user['userid'],
-                        'time_afk'   => $user['time_afk'],
-                        'alasan' => $user['alasan'],
-                        'username' => $user['username']
-                    );
-
-                    $jsonfile = json_encode($data, JSON_PRETTY_PRINT);
-                    $anggota = file_put_contents($file, $jsonfile);
-
-                    $reply = "Maaf ," . $afkforstname . ' ' . $afklastname . ' sedang AFK sejak ' . $user['time_afk'] . PHP_EOL .
-                        'Alasan : ' . $user['alasan'];
-                    $content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
-                    $telegram->sendMessage($content);
-                    exit;
                 }
+            } else {
             }
-        } else {
         }
     }
 }
