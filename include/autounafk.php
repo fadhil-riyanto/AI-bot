19e9a9fa7caf5b47acb74bd03ee43b4ad37d620b
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-03-26 14:05:41 +0700

    update perbaikan

--- include/autounafk.php
+++ include/autounafk.php
@@ -11,14 +11,40 @@ function deteksi_grupP()
 }
 if (deteksi_grupP() == true) {
 
-    $db = new MysqliDb(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
-    $db->where('userid', $userID);
-    $user = $db->getOne("afk_user_data");
 
-    if ($cekher_diri != NULL) {
+    $delaycached = 20;
+    $getlistjsonadmin = __DIR__ . '/../json_data/afkstatus.json';
+    function GETuseridafk($userID)
+    {
+        global $getlistjsonadmin, $timed, $alasan, $uid_afk;
+        $anggota = file_get_contents($getlistjsonadmin);
+        $data = json_decode($anggota, true);
+        foreach ($data as $d) {
+            if ($d['userid'] == $userID) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    $cekher_diri = GETuseridafk($userID);
+    if ($cekher_diri == true) {
         $reply = "anda sudah tidak afk";
         $content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
         $telegram->sendMessage($content);
+
+        $file = __DIR__ . '/../json_data/afkstatus.json';
+        $anggota = file_get_contents($file);
+        $data = json_decode($anggota, true);
+
+        foreach ($data as $key => $d) {
+            if ($d['userid'] === $userID) {
+                array_splice($data, $key, 1);
+            }
+        }
+
+        $jsonfile = json_encode($data, JSON_PRETTY_PRINT);
+        $anggota = file_put_contents($file, $jsonfile);
 
         $db = new MysqliDb(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
         $db->where('userid', $userID);
