3bae4a107da25febca1f5d4b957ab21776773e7c
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-03-26 13:49:27 +0700

    update perbaikan

--- group_command/afk.php
+++ group_command/afk.php
@@ -10,9 +10,76 @@ $result = $telegram->getData();
 $azanHilangcommand = str_replace($adanParse[0], '', $text);
 $udahDiparse = str_replace($adanParse[0] . ' ', '', $text);
 if ($azanHilangcommand == null) {
-    $reply = "ups, anda harus memasukkan alasan anda afk.";
-    $content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
-    $telegram->sendMessage($content);
+    $db = new MysqliDb(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+    $db->where("userid", $userID);
+    $user = $db->getOne("afk_user_data");
+    if ($user['userid'] == null) {
+        $timed = date("H:i:s");
+        $data = array(
+            "userid" => $userID,
+            "time_afk" =>  $timed,
+            "alasan" => 'pengguna ini afk tanpa memberi alasan',
+            "username" => $usernameBelumdiparse
+        );
+
+
+        $id = $db->insert('afk_user_data', $data);
+        if ($id) {
+            $file = __DIR__ . '/../json_data/afkstatus.json';
+            $anggota = file_get_contents($file);
+            $datass = json_decode($anggota, true);
+
+            $datass[] = array(
+                "userid" => $userID,
+                "time_afk" =>  $timed,
+                "alasan" => $udahDiparse,
+                "username" => $usernameBelumdiparse
+            );
+
+            $jsonfile = json_encode($datass, JSON_PRETTY_PRINT);
+            $anggota = file_put_contents($file, $jsonfile);
+
+            $reply = "anda sedang afk!" . PHP_EOL . 'gunakan /unafk untuk melepas status afk!';
+            $content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
+            $telegram->sendMessage($content);
+        } else {
+            $reply = "ups ada kesalahan internal. Reason : " . $db->getLastError();
+            $content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
+            $telegram->sendMessage($content);
+        }
+    } else {
+        $data = array(
+            "userid" => $userID,
+            "time_afk" =>  date("H:i:s"),
+            "alasan" => 'pengguna ini afk tanpa memberi alasan',
+            "username" => $usernameBelumdiparse
+            // active = !active;
+        );
+        $db->where('userid', $userID);
+        if ($db->update('afk_user_data', $data)) {
+
+            $file = __DIR__ . '/../json_data/afkstatus.json';
+            $anggota = file_get_contents($file);
+            $datass = json_decode($anggota, true);
+
+            $datass[] = array(
+                "userid" => $userID,
+                "time_afk" =>  $timed,
+                "alasan" => $udahDiparse,
+                "username" => $usernameBelumdiparse
+            );
+
+            $jsonfile = json_encode($datass, JSON_PRETTY_PRINT);
+            $anggota = file_put_contents($file, $jsonfile);
+            $reply = "anda sedang afk!" . PHP_EOL . 'gunakan /unafk untuk melepas status afk!';
+            $content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
+            $telegram->sendMessage($content);
+        } else {
+            $reply = "ups, ada kesalahan!. Penyebab : " . $db->getLastError();
+            $content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
+            $telegram->sendMessage($content);
+        }
+    }
 } else {
     $db = new MysqliDb(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
     $db->where("userid", $userID);
