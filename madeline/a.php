958253e79e3e6d9229e064c63f3aa4a6ba7b949a
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-04-02 19:11:32 +0700

    add madeline

--- madeline/a.php
+++ madeline/a.php
@@ -1,27 +1,25 @@
 <?php
-if(isset($_GET['u'])){
-	
-if (!file_exists('madeline.php')) {
-    copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
-}
-include 'madeline.php';
+if (isset($_GET['u'])) {
 
-$MadelineProto = new \danog\MadelineProto\API('session.madeline');
-$MadelineProto->async(true);
-$MadelineProto->loop(function () use ($MadelineProto) {
-    yield $MadelineProto->start();
-
-    $me = yield $MadelineProto->getSelf();
-
-    $MadelineProto->logger($me);
-
-    if (!$me['bot']) {
-			$Vector_of_User = yield $MadelineProto->users->getUsers(['id' => $_GET['u']]);
-			//file_put_contents('ngetes.txt', );
-			echo json_encode($Vector_of_User, JSON_PRETTY_PRINT);
+    if (!file_exists('madeline.php')) {
+        copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
     }
-    //yield $MadelineProto->echo('OK, done!');
-});
+    include 'madeline.php';
 
+    $MadelineProto = new \danog\MadelineProto\API('session.madeline');
+    $MadelineProto->async(true);
+    $MadelineProto->loop(function () use ($MadelineProto) {
+        yield $MadelineProto->start();
 
+        $me = yield $MadelineProto->getSelf();
+
+        $MadelineProto->logger($me);
+
+        if (!$me['bot']) {
+            $Vector_of_User = yield $MadelineProto->users->getUsers(['id' => [$_GET['u']]]);
+            //file_put_contents('ngetes.txt', );
+            echo json_encode($Vector_of_User, JSON_PRETTY_PRINT);
+        }
+        //yield $MadelineProto->echo('OK, done!');
+    });
 }
