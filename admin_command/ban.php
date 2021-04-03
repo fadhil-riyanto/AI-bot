76feb618abc64a1d0673a00620a14d1f28a539e3
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-04-03 23:44:33 +0700

    buat function username resolver

--- admin_command/ban.php
+++ admin_command/ban.php
@@ -4,22 +4,30 @@ $promote_uid = $tgpromote['message']['reply_to_message']['from']['id'];
 $fname_depan = $tgpromote['message']['reply_to_message']['from']['first_name'];
 $lname_blakang = $tgpromote['message']['reply_to_message']['from']['last_name'];
 
+$unamepromote = '<a href="tg://user?id=' . $promote_uid . '">' . $fname_depan . ' ' . $lname_blakang . '</a>';
+
 $azanHilangcommand = str_replace($adanParse_plain[0], '', $text_plain);
 $udahDiparse = str_replace($adanParse_plain[0] . ' ', '', $text_plain);
 $udahDiparse_hash = str_replace($adanParse_plain[0] . ' ', '', $text_plain_nokarakter);
 
-if ($azanHilangcommand == null || !isset($promote_uid)) {
+if (isset($promote_uid)) {
+    $noreply = $promote_uid;
+} elseif ($udahDiparse != null) {
+    $h = username_resolver($udahDiparse);
+    $noreply = $h['id'];
+} elseif ($udahDiparse == null) {
+    $noreply = null;
+}
+
+if ($noreply == null) {
     $reply = 'Hai ' . $username . PHP_EOL . 'maaf, untuk membanned user. anda bisa mention ke user yang ingin di banned atau mereply pesan nya';
     $content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
     $telegram->sendMessage($content);
 } else {
-    if ($promote_uid == null) {
-        $h = username_resolver($udahDiparse);
-        $promote_uid = $h['id'];
-    }
+
     $param_promote = array(
         'chat_id' => $chat_id,
-        'user_id' => $promote_uid
+        'user_id' => $noreply
     );
 
     $telegram->kickChatMember($param_promote);
