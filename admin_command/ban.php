4d405e72f3cae2f25b93b96b6dff8ba6815601ca
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-04-03 23:25:19 +0700

    buat function username resolver

--- admin_command/ban.php
+++ admin_command/ban.php
@@ -1,42 +1,59 @@
+<?php
+$tgpromote = $telegram->getData();
+$promote_uid = $tgpromote['message']['reply_to_message']['from']['id'];
+$fname_depan = $tgpromote['message']['reply_to_message']['from']['first_name'];
+$lname_blakang = $tgpromote['message']['reply_to_message']['from']['last_name'];
 
+$azanHilangcommand = str_replace($adanParse_plain[0], '', $text_plain);
+$udahDiparse = str_replace($adanParse_plain[0] . ' ', '', $text_plain);
+$udahDiparse_hash = str_replace($adanParse_plain[0] . ' ', '', $text_plain_nokarakter);
 
-    <?php
-    $tgpromote = $telegram->getData();
-    $promote_uid = $tgpromote['message']['reply_to_message']['from']['id'];
-    $fname_depan = $tgpromote['message']['reply_to_message']['from']['first_name'];
-    $lname_blakang = $tgpromote['message']['reply_to_message']['from']['last_name'];
-
-    $unamepromote = '<a href="tg://user?id=' . $promote_uid . '">' . $fname_depan . ' ' . $lname_blakang . '</a>';
-
-
-    $azanHilangcommand = str_replace($adanParse_plain[0], '', $text_plain);
-    $udahDiparse = str_replace($adanParse_plain[0] . ' ', '', $text_plain);
-    if (isset($promote_uid)) {
-        $param_promote = array(
-            'chat_id' => $chat_id,
-            'user_id' => $promote_uid
-        );
-
-        $telegram->kickChatMember($param_promote);
-
-        $reply = $unamepromote . ', dibanned!!.';
-        $content = array('chat_id' => $chat_id, 'text' => $reply, 'parse_mode' => 'html', 'reply_to_message_id' => $message_id, 'disable_web_page_preview' => true);
-        $telegram->sendMessage($content);
-
-
-        //debug
-        // $reply = $unamepromote . ', dibanned!!.';
-        // $content = array('chat_id' => $chat_id, 'text' => $reply, 'parse_mode' => 'html', 'reply_to_message_id' => $message_id, 'disable_web_page_preview' => true);
-        // $telegram->sendMessage($content);
-        //unban lagi
-        $param_promote = array(
-            'chat_id' => $chat_id,
-            'user_id' => $promote_uid
-        );
-        $telegram->unbanChatMember($param_promote);
-    } else {
-
-        $reply = 'ups, anda harus mereply user yang ingin dibanned.';
-        $content = array('chat_id' => $chat_id, 'text' => $reply, 'parse_mode' => 'html', 'reply_to_message_id' => $message_id, 'disable_web_page_preview' => true);
-        $telegram->sendMessage($content);
+if ($azanHilangcommand == null || !isset($promote_uid)) {
+    $reply = 'Hai ' . $username . PHP_EOL . 'maaf, untuk membanned user. anda bisa mention ke user yang ingin di banned atau mereply pesan nya';
+    $content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
+    $telegram->sendMessage($content);
+} else {
+    if ($promote_uid == null) {
+        $h = username_resolver($udahDiparse);
+        $promote_uid = $h['id'];
     }
+    $param_promote = array(
+        'chat_id' => $chat_id,
+        'user_id' => $promote_uid
+    );
+
+    $telegram->kickChatMember($param_promote);
+
+    $reply = $unamepromote . ', dibanned!!.';
+    $content = array('chat_id' => $chat_id, 'text' => $reply, 'parse_mode' => 'html', 'reply_to_message_id' => $message_id, 'disable_web_page_preview' => true);
+    $telegram->sendMessage($content);
+
+
+    //debug
+    // $reply = $unamepromote . ', dibanned!!.';
+    // $content = array('chat_id' => $chat_id, 'text' => $reply, 'parse_mode' => 'html', 'reply_to_message_id' => $message_id, 'disable_web_page_preview' => true);
+    // $telegram->sendMessage($content);
+    //unban lagi
+    $param_promote = array(
+        'chat_id' => $chat_id,
+        'user_id' => $promote_uid
+    );
+    $telegram->unbanChatMember($param_promote);
+}
+
+
+
+
+// $unamepromote = '<a href="tg://user?id=' . $promote_uid . '">' . $fname_depan . ' ' . $lname_blakang . '</a>';
+
+
+// $azanHilangcommand = str_replace($adanParse_plain[0], '', $text_plain);
+// $udahDiparse = str_replace($adanParse_plain[0] . ' ', '', $text_plain);
+// if (isset($promote_uid)) {
+    
+// } else {
+
+//     $reply = 'ups, anda harus mereply user yang ingin dibanned.';
+//     $content = array('chat_id' => $chat_id, 'text' => $reply, 'parse_mode' => 'html', 'reply_to_message_id' => $message_id, 'disable_web_page_preview' => true);
+//     $telegram->sendMessage($content);
+// }
--- command/debug.php
+++ command/debug.php
@@ -1,7 +1,7 @@
 <?php
 $udahDiparse = str_replace($adanParse[0] . ' ', '', $text);
 
-$h = username_resolver($udahDiparse);
-$reply = $h['id'];
-$content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
-$telegram->sendMessage($content);
+// $h = username_resolver($udahDiparse);
+// $reply = $h['id'];
+// $content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
+// $telegram->sendMessage($content);
