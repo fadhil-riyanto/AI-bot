b3ce102bb46f4618cfc07af7246aa487ce4851db
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-04-04 11:41:20 +0700

    ubah apis

--- command/tulis.php
+++ command/tulis.php
@@ -1,21 +1,21 @@
 <?php
 //$hapusTulis = 
-	$udahDiparse = str_replace($adanParse_plain[0] . ' ', '', $text_plain);
-	$curlImage = file_get_contents('http://salism3.pythonanywhere.com/write?text=' . urlencode($udahDiparse));
-	$parsecJsons = json_decode($curlImage);
-	if ($adanParse[1] == null) {
-		$reply = 'Hai ' . $username . PHP_EOL . 'Maaf, Pattern kosong, gunakan format' . PHP_EOL . PHP_EOL . '<pre>/tulis {your text}</pre>';
-		$content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
-		$telegram->sendMessage($content);
-		exit;
-	} elseif ($parsecJsons->message == "Sukses !! gambar akan dihapus dalam 100 detik") {
-		$konten = array('chat_id' => $chat_id, 'photo' => $parsecJsons->images[0], 'caption' => 'Hai ' . $username . ', Gambar berhasil dibuat!', 'reply_to_message_id' => $message_id,);
-		$telegram->sendPhoto($konten);
+$udahDiparse = str_replace($adanParse_plain[0] . ' ', '', $text_plain);
+$curlImage = file_get_contents('http://salism3.pythonanywhere.com/write/?text=' . urlencode($udahDiparse));
+$parsecJsons = json_decode($curlImage);
+if ($adanParse[1] == null) {
+	$reply = 'Hai ' . $username . PHP_EOL . 'Maaf, Pattern kosong, gunakan format' . PHP_EOL . PHP_EOL . '<pre>/tulis {your text}</pre>';
+	$content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
+	$telegram->sendMessage($content);
+	exit;
+} elseif ($parsecJsons->success == true) {
+	$konten = array('chat_id' => $chat_id, 'photo' => $parsecJsons->images[0], 'caption' => 'Hai ' . $username . ', Gambar berhasil dibuat!', 'reply_to_message_id' => $message_id,);
+	$telegram->sendPhoto($konten);
 
-		exit;
-	} else {
-		$reply = 'Hai ' . $username . PHP_EOL . 'Maaf, sepertinya ada yang error di sistem kami';
-		$content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
-		$telegram->sendMessage($content);
-		exit;
-	}
+	exit;
+} else {
+	$reply = 'Hai ' . $username . PHP_EOL . 'Maaf, sepertinya ada yang error di sistem kami';
+	$content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
+	$telegram->sendMessage($content);
+	exit;
+}
