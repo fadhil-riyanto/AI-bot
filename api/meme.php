1995d840dba4d38fc6bc2c370cd68c6b756a1e41
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-04-07 07:53:24 +0700

    add curl, dia ngeblock heroku

--- api/meme.php
+++ api/meme.php
@@ -1,10 +1,9 @@
 <?php
 require __DIR__ . '/middleware.php';
 
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://docs-api-zahirrr.herokuapp.com/api/meme");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $output = curl_exec($ch);
-    curl_close($ch);
-    render_json(json_decode($output));
-
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://textmakes.anonymoususer18.repl.co/meme.php");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$output = curl_exec($ch);
+curl_close($ch);
+render_json(json_decode($output));
