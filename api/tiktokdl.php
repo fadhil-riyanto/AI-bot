72980c63b15bb1707e598a1c3edc1e48ba9319bd
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-04-09 21:39:11 +0700

    add tiktok

--- api/tiktokdl.php
+++ api/tiktokdl.php
@@ -2,50 +2,14 @@
 require __DIR__ . '/middleware.php';
 
 if (isset($_GET['url'])) {
-    require __DIR__ . '/../include/simple_html_dom.php';
-    try {
-        //code...
-        $client = new \GuzzleHttp\Client();
 
-        $response = $client->request('POST', 'https://snaptik.app/action-2021.php?lang=EN', [
-            'form_params' => [
-                'url' => $_GET['url']
-            ]
-        ]);
-        $htmls =  $response->getBody();
-        echo $htmls;
-        die();
-        //dapatkan button link
-        $html = new simple_html_dom();
-        $html->load($htmls);
-        $text_buttonlink = $html->find('div[class=abuttons]', 0)->innertext;
-        //get value href
-        preg_match_all('/<a[^>]+href=([\'"])(?<href>.+?)\1[^>]*>/i', $text_buttonlink, $matc_link_button);
-        $a_link_tiktok = $matc_link_button['href'];
-
-
-        $html = new simple_html_dom();
-        $html->load($htmls);
-        $text_img = $html->find('div[class=zhay-left left]', 0)->innertext;
-        //preg_match_all('@src="([^"]+)"@', $text, $match_image);
-
-        //ekstrak linh
-        preg_match_all('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $text_img, $match_gambar);
-
-
-        $captionnn = $html->find('div[class=zhay-middle center]', 0)->innertext;
-        $kapsen_hehe = strip_tags($captionnn);
-        render_json(array(
-            "downloadlink" => $a_link_tiktok,
-            "thumb" => $match_gambar[0],
-            "caption" => $kapsen_hehe
-        ), JSON_PRETTY_PRINT);
-    } catch (\GuzzleHttp\Exception\ServerException $th) {
-        //throw $th;
-        render_json(array(
-            "error" => "intenal sistem"
-        ), JSON_PRETTY_PRINT);
-    }
+    $br = $_GET['url'];
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https: //textmakes.anonymoususer18.repl.co/tiktok.php?url=" . urlencode($br));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    render_json(json_decode($output));
 } else {
     render_json(array(
         "error" => "parameter url dibutuhkan"
