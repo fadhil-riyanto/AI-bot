adbcd50c445676902df8160ad883bfdc61fceee7
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-04-18 14:14:32 +0700

    update engine dan kasi cache

--- hook.php
+++ hook.php
@@ -68,19 +68,19 @@ try {
 
 
 	//debug mode
-	if (detect_grup() == null) {
-		if ($userID != $userid_pemilik) {
-			$reply = "Maaf, bot ini sedang dalam pengembangan lebih lanjut oleh " . PUMBUAT_BOT . PHP_EOL .
-				"Coba lagi nanti";
-			$content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
-			$telegram->sendMessage($content);
-			exit;
-		}
-	} else {
-		if ($userID != $userid_pemilik) {
-			die();
-		}
-	}
+	// if (detect_grup() == null) {
+	// 	if ($userID != $userid_pemilik) {
+	// 		$reply = "Maaf, bot ini sedang dalam pengembangan lebih lanjut oleh " . PUMBUAT_BOT . PHP_EOL .
+	// 			"Coba lagi nanti";
+	// 		$content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
+	// 		$telegram->sendMessage($content);
+	// 		exit;
+	// 	}
+	// } else {
+	// 	if ($userID != $userid_pemilik) {
+	// 		die();
+	// 	}
+	// }
 
 
 	$hilangAzan = str_replace('/azan ', '', $text, $hit);
