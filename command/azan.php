430b9ceb48356fb1615b80dae1b071650fd28683
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-04-23 09:55:51 +0700

    percobaan mass 1

--- command/azan.php
+++ command/azan.php
@@ -2,7 +2,7 @@
 $azanHilangcommand = str_replace($adanParse[0], '', $text);
 $udahDiparse = str_replace($adanParse[0] . ' ', '', $text);
 if ($azanHilangcommand != null) {
-	$azanDapatkankota = file_get_contents('https://api.banghasan.com/sholat/format/json/kota/nama/' . urlencode($udahDiparse));
+	$azanDapatkankota = file_get_contents('https://api.banghasan.com/sholat/format/json/kota/nama/' . urlencode(str_replace('city', 'kota', $udahDiparse)));
 	$h = json_decode($azanDapatkankota);
 	if ($h->kota != null) {
 		foreach ($h->kota as $azanKode) {
@@ -10,16 +10,7 @@ if ($azanHilangcommand != null) {
 				//echo $azanKode->id;
 				$azangetJadwal = file_get_contents('https://api.banghasan.com/sholat/format/json/jadwal/kota/' . $azanKode->id . '/tanggal/' . date('Y-m-d'));
 				$jsonHasilazancurl = json_decode($azangetJadwal);
-				$reply = 'Hai ' . $username . ', Jadwal azan dikota ' . strtolower($udahDiparse) . ' Adalah' . PHP_EOL . PHP_EOL .
-					'subuh : ' . $jsonHasilazancurl->jadwal->data->subuh . PHP_EOL .
-					'imsak : ' . $jsonHasilazancurl->jadwal->data->imsak . PHP_EOL .
-					'terbit : ' . $jsonHasilazancurl->jadwal->data->terbit . PHP_EOL .
-					'dhuha : ' . $jsonHasilazancurl->jadwal->data->dhuha . PHP_EOL .
-					'dzuhur : ' . $jsonHasilazancurl->jadwal->data->dzuhur . PHP_EOL .
-					'ashar : ' . $jsonHasilazancurl->jadwal->data->ashar . PHP_EOL .
-					'maghrib : ' . $jsonHasilazancurl->jadwal->data->maghrib . PHP_EOL .
-					'isya : ' . $jsonHasilazancurl->jadwal->data->isya . PHP_EOL .
-					'tanggal : ' . $jsonHasilazancurl->jadwal->data->tanggal;
+				$reply = bahasa('modul_azan_sukses');
 				$content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
 				$telegram->sendMessage($content);
 				exit;
@@ -47,7 +38,7 @@ if ($azanHilangcommand != null) {
 		exit;
 	}
 } elseif ($azanHilangcommand == null) {
-	$reply = 'Maaf, gunakan pattern <pre>/azan nama kota</pre>' . PHP_EOL . PHP_EOL . 'contoh : /azan jakarta';
+	$reply = bahasa('modul_azan_pattern_kosong');
 	$content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
 	$telegram->sendMessage($content);
 }
