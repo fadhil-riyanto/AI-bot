e7964ef1ea14e58f56a2b5b1fb1241391b58ddaf
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-04-25 05:40:52 +0700

    updates prevs

--- admin_command/__init__.php
+++ admin_command/__init__.php
@@ -154,6 +154,7 @@ if (deteksi_grup() == true) {
             $reply = 'maaf, saya tidak bisa melakukan aksi yang anda perintahkan karna saya bukan admin grup ini.';
             $content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
             $telegram->sendMessage($content);
+            die();
         } else {
         }
     }
