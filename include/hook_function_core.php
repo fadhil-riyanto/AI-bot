39fc84e5a0e27761a73781bf184a3c98bab65054
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-05-07 00:54:49 +0700

    tambah func unlink file, thanks https://stackoverflow.com/a/44350047/14812963

--- include/hook_function_core.php
+++ include/hook_function_core.php
@@ -175,3 +175,23 @@ function cleanStrings_dari_utf8($text)
 	);
 	return preg_replace(array_keys($utf8), array_values($utf8), $text);
 }
+
+
+function unlinkFile($filename)
+{
+	// try to force symlinks
+	if (is_link($filename)) {
+		$sym = @readlink($filename);
+		if ($sym) {
+			return is_writable($filename) && @unlink($filename);
+		}
+	}
+
+	// try to use real path
+	if (realpath($filename) && realpath($filename) !== $filename) {
+		return is_writable($filename) && @unlink(realpath($filename));
+	}
+
+	// default unlink
+	return is_writable($filename) && @unlink($filename);
+}
