5220e5bb47bd5fc0e97511d43a2da5b5b6385645
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-05-14 20:06:56 +0700

    rubah teks

--- command/reverse.php
+++ command/reverse.php
@@ -40,7 +40,7 @@ if (isset($fileid)) {
     $dirTgid = $getinfoDir['result']['file_path'];
     $downloadingimg = downloadUrlToFile('https://api.telegram.org/file/bot' . TG_HTTP_API . '/' . $dirTgid, 'tmp/pict_uploader' . $randomTokenss . '.png');
 } else {
-    $reply = 'maaf, anda harus membalas foto photo';
+    $reply = 'maaf, anda harus membalas command dengan foto';
     $content = array('chat_id' => $chat_id, 'text' => $reply, 'reply_to_message_id' => $message_id, 'parse_mode' => 'html', 'disable_web_page_preview' => true);
     $editmsg = $telegram->sendMessage($content);
     exit;
