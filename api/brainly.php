73f599d5a2dfd9762902ef5698131a7d3c6e58c6
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-05-28 13:45:01 +0700

    fixed brainly

--- api/brainly.php
+++ api/brainly.php
@@ -3,7 +3,7 @@ require __DIR__ . '/middleware.php';
 if (isset($_GET['query'])) {
     $br = $_GET['query'];
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://server-data.000webhostapp.com/brainly.php?query=" . urlencode($br));
+    curl_setopt($ch, CURLOPT_URL, "http://fadhilsss.eu5.org/brainly.php?q=" . urlencode($br));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $output = curl_exec($ch);
     curl_close($ch);
