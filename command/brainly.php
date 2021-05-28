ec64fa81bb566727abcab3af336367e07542a4a1
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2021-05-28 09:56:04 +0700

    fixed brainly

--- command/brainly.php
+++ command/brainly.php
@@ -14,7 +14,7 @@ if ($azanHilangcommand == null) {
     $telegram->sendMessage($content);
 } else {
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://server-data.000webhostapp.com/brainly.php?query=" . urlencode($udahDiparse));
+    curl_setopt($ch, CURLOPT_URL, "http://fadhilriyanto.rf.gd/rmf.php?q=" . urlencode($udahDiparse));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $output = curl_exec($ch);
     curl_close($ch);
